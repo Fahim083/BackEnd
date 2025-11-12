04e32f44a7633786f64a3ccf439bea4ba374c2f8
Fahim083 <fahimhaowlader07@gmail.com>
2025-11-12 18:44:39 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+import express from 'express'
+import cors from 'cors'
+const app = express()
+import dotenv from 'dotenv'
+import { MongoClient, ServerApiVersion,ObjectId } from 'mongodb';
+const port = process.env.PORT || 3000
+dotenv.config()
+
+app.use(cors({
+  origin: ['http://localhost:3000',
+   'http://localhost:5173',
+  'http://localhost:5173',
+  'https://velvety-moxie-01f024.netlify.app'
+  ],
+  credentials: true
+}))
+app.use(express.json()) 
+
+
+
+app.get("/",async(req,res)=>{
+    res.send("Hello World")
+})
+
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.2n5zm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+
+
+    const PropertyCollection = client.db('Homenest').collection('Properties');
+    const ReviewCollection = client.db('Homenest').collection('Reviews');
+
+
+  }  finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+})
+
