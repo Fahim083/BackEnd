9e912c263c120c0c658a9397603f5874a2c20e5c
Fahim083 <fahimhaowlader07@gmail.com>
2025-11-13 19:10:19 +0600

    problem fixed

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors({
   origin: ['http://localhost:3000',
    'http://localhost:5173',
   'http://localhost:5173',
-  'homenests.netlify.app',
+  'https://homenests.netlify.app/home',
   'https://velvety-moxie-01f024.netlify.app'
   ],
   credentials: true
